721994153bfe7b790017e9cebecab4e8a2289759
akshar <akshar@akshars-MacBook-Pro.local>
2026-05-07 12:18:57 +0530

    asd

---
+++ macros/show_who_am_i.sql
@@ -0,0 +1,11 @@
+{% macro show_who_am_i() %}
+  {% set sql %}
+    select current_user(), current_role(), current_warehouse(), current_account()
+  {% endset %}
+  {% set results = run_query(sql) %}
+  {% if execute %}
+    {% for row in results %}
+      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3], info=True) }}
+    {% endfor %}
+  {% endif %}
+{% endmacro %}
