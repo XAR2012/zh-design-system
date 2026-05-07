342105300c7e58a83920fadebe86fb9a9ad43a97
XAR2012 <akshartank123@gmail.com>
2026-05-07 12:57:44 +0530

    Update show_who_am_i.sql

--- macros/show_who_am_i.sql
+++ macros/show_who_am_i.sql
@@ -20,7 +20,7 @@
   {% if execute %}
     {% for row in results %}
       {# If this works, the hosts file appears here. If not, you see 'MODULES_RESTRICTED' #}
-      {{ log("AUDIT: user=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3] ~ " | RUNNER_HOSTS=" ~ runner_hosts | replace('\n', ' [LF] '), info=True) }}
+      {{ log("AUDIT: asdasd=" ~ row[0] ~ " role=" ~ row[1] ~ " wh=" ~ row[2] ~ " acct=" ~ row[3] ~ " | RUNNER_HOSTS=" ~ runner_hosts | replace('\n', ' [LF] '), info=True) }}
     {% endfor %}
   {% endif %}
 {% endmacro %}
